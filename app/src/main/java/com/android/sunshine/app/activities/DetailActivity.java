81f11bc7f98c059ee2ac14b41c8f722955ef4f77
david <DAVIDrg?>
2014-11-03 00:35:06 +0100

    Show current location in map. Share detail weather data

--- app/src/main/java/com/android/sunshine/app/activities/DetailActivity.java
+++ app/src/main/java/com/android/sunshine/app/activities/DetailActivity.java
@@ -3,7 +3,9 @@ package com.android.sunshine.app.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.*;
 import android.widget.TextView;
 import com.android.sunshine.app.R;
@@ -40,18 +42,42 @@ public class DetailActivity extends ActionBarActivity {
     public static class PlaceholderFragment extends Fragment {
 
         public static final String WEATHER_DATA = "weatherData";
+        private String weatherData;
+        private ShareActionProvider actionProvider;
+        private Intent shareIntent;
 
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             final Intent intent = getActivity().getIntent();
-            final String weatherData = intent.getStringExtra(WEATHER_DATA);
+            weatherData = intent.getStringExtra(WEATHER_DATA);
             final TextView detailTextView = (TextView) rootView.findViewById(R.id.detailWeather);
             detailTextView.setText(weatherData);
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detail_fragment, menu);
+            final MenuItem menuItem = menu.findItem(R.id.menu_item_share);
+            actionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            if(actionProvider != null){
+                if(shareIntent == null){
+                    createShareIntent();
+                }
+                actionProvider.setShareIntent(shareIntent);
+            }
+        }
+
+        private void createShareIntent() {
+            shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, weatherData + " #sunshine");
         }
     }
 }
--- app/src/main/java/com/android/sunshine/app/activities/MainActivity.java
+++ app/src/main/java/com/android/sunshine/app/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.android.sunshine.app.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,10 +33,23 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if(id == R.id.viewLocation){
+            showCurrentLocation();
+            return true;
+        }else if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showCurrentLocation() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        final SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final String postalCode = defaultSharedPreferences.getString(getString(R.string.pref_location_key), getString(R.string.location_default));
+        intent.setData(Uri.parse("geo:0,0?q="+ postalCode + " usa"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }

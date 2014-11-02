7ad0d3f5ecc1f4f86fda0747d0447f05f55bfc3e
david <DAVIDrg?>
2014-11-02 08:47:37 +0100

    Add refresh menu item

--- app/src/main/java/com/android/sunshine/app/fragments/ForecastFragment.java
+++ app/src/main/java/com/android/sunshine/app/fragments/ForecastFragment.java
@@ -2,9 +2,7 @@ package com.android.sunshine.app.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import com.android.sunshine.app.R;
 
 public class ForecastFragment extends Fragment {
@@ -13,6 +11,12 @@ public class ForecastFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.forecast_fragment, menu);
     }
 }

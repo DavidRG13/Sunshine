fad4c6344c86f17ec2146eb12bd352299bdbdc97
david <DAVIDrg?>
2014-11-11 22:43:42 +0100

    [WIP] Creating tablet layout, not showing details fragment

--- app/src/main/java/com/android/sunshine/app/activities/MainActivity.java
+++ app/src/main/java/com/android/sunshine/app/activities/MainActivity.java
@@ -9,18 +9,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.android.sunshine.app.R;
-import com.android.sunshine.app.fragments.ForecastFragment;
+import com.android.sunshine.app.fragments.DetailFragment;
 
 public class MainActivity extends ActionBarActivity {
+
+    private boolean twoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
-                    .commit();
+        if (findViewById(R.id.fragment_detail_container) != null) {
+            twoPane = true;
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_detail_container, new DetailFragment())
+                        .commit();
+            }
+        } else {
+            twoPane = false;
         }
     }
 
@@ -33,10 +40,10 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if(id == R.id.viewLocation){
+        if (id == R.id.viewLocation) {
             showCurrentLocation();
             return true;
-        }else if (id == R.id.action_settings) {
+        } else if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
@@ -47,7 +54,7 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         final SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         final String postalCode = defaultSharedPreferences.getString(getString(R.string.pref_location_key), getString(R.string.location_default));
-        intent.setData(Uri.parse("geo:0,0?q="+ postalCode + " usa"));
+        intent.setData(Uri.parse("geo:0,0?q=" + postalCode + " usa"));
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }

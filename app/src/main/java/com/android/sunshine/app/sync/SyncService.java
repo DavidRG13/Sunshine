34b33787e1635b7f44342bb17e36f20fe2b31de5
david <DAVIDrg?>
2014-12-28 18:17:36 +0100

    Clean colors

--- app/src/main/java/com/android/sunshine/app/sync/SyncService.java
+++ app/src/main/java/com/android/sunshine/app/sync/SyncService.java
@@ -7,19 +7,19 @@ import android.os.IBinder;
 public class SyncService extends Service{
 
     private static final Object sSyncAdapterLock = new Object();
-    private static SyncAdapter sSunshineSyncAdapter = null;
+    private static SyncAdapter syncAdapter = null;
 
     @Override
     public void onCreate() {
         synchronized (sSyncAdapterLock) {
-            if (sSunshineSyncAdapter == null) {
-                sSunshineSyncAdapter = new SyncAdapter(getApplicationContext(), true);
+            if (syncAdapter == null) {
+                syncAdapter = new SyncAdapter(getApplicationContext(), true);
             }
         }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return sSunshineSyncAdapter.getSyncAdapterBinder();
+        return syncAdapter.getSyncAdapterBinder();
     }
 }

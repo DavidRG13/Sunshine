1778d9dc645d62b5c84a42c8fd3e8feffbc11861
david <DAVIDrg?>
2015-06-02 09:17:25 +0200

    Make the widget horizontally resizable

--- app/src/main/java/com/android/sunshine/app/widget/TodayWidgetIntentService.java
+++ app/src/main/java/com/android/sunshine/app/widget/TodayWidgetIntentService.java
@@ -9,6 +9,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.widget.RemoteViews;
 import com.android.sunshine.app.R;
 import com.android.sunshine.app.activities.MainActivity;
@@ -20,12 +23,14 @@ public class TodayWidgetIntentService extends IntentService {
     private static final String[] FORECAST_COLUMNS = {
         WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
         WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
-        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP
+        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+        WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
     };
     // these indices must match the projection
     private static final int INDEX_WEATHER_ID = 0;
     private static final int INDEX_SHORT_DESC = 1;
     private static final int INDEX_MAX_TEMP = 2;
+    private static final int INDEX_MIN_TEMP = 3;
 
     public TodayWidgetIntentService() {
         super("TodayWidgetIntentService");
@@ -37,7 +42,8 @@ public class TodayWidgetIntentService extends IntentService {
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, TodayWidgetProvider.class));
 
         String location = Utilities.getLocationSettings(this);
-        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(location, System.currentTimeMillis());
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+            location, System.currentTimeMillis());
         Cursor data = getContentResolver().query(weatherForLocationUri, FORECAST_COLUMNS, null,
             null, WeatherContract.WeatherEntry.COLUMN_DATE + " ASC");
         if (data == null) {
@@ -52,18 +58,32 @@ public class TodayWidgetIntentService extends IntentService {
         int weatherArtResourceId = Utilities.getArtResourceForWeatherCondition(weatherId);
         String description = data.getString(INDEX_SHORT_DESC);
         double maxTemp = data.getDouble(INDEX_MAX_TEMP);
+        double minTemp = data.getDouble(INDEX_MIN_TEMP);
         String formattedMaxTemperature = Utilities.formatTemperature(this, maxTemp);
+        String formattedMinTemperature = Utilities.formatTemperature(this, minTemp);
         data.close();
 
         for (int appWidgetId : appWidgetIds) {
-            int layoutId = R.layout.widget_today_small;
+            int widgetWidth = getWidgetWidth(appWidgetManager, appWidgetId);
+            int defaultWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+            int largeWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_large_width);
+            int layoutId;
+            if (widgetWidth >= largeWidth) {
+                layoutId = R.layout.widget_today_large;
+            } else if (widgetWidth >= defaultWidth) {
+                layoutId = R.layout.widget_today;
+            } else {
+                layoutId = R.layout.widget_today_small;
+            }
             RemoteViews views = new RemoteViews(getPackageName(), layoutId);
 
             views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
                 setRemoteContentDescription(views, description);
             }
+            views.setTextViewText(R.id.widget_description, description);
             views.setTextViewText(R.id.widget_high_temperature, formattedMaxTemperature);
+            views.setTextViewText(R.id.widget_low_temperature, formattedMinTemperature);
 
             Intent launchIntent = new Intent(this, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
@@ -71,6 +91,24 @@ public class TodayWidgetIntentService extends IntentService {
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+    }
+
+    private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            return getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+        }
+        return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId) {
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        if (options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)) {
+            int minWidthDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidthDp, displayMetrics);
+        }
+        return  getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)

36111d47cbc2e5b6479b24774f4ba2a0be35a86a
david <DAVIDrg?>
2015-07-16 20:27:58 +0100

    Test ServerStatusChanger

---
+++ app/src/androidTest/java/com/android/sunshine/app/utils/ServerStatusChangerTest.java
@@ -0,0 +1,37 @@
+package com.android.sunshine.app.utils;
+
+import android.test.AndroidTestCase;
+import com.android.sunshine.app.sync.ServerStatus;
+import java.net.HttpURLConnection;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ServerStatusChangerTest extends AndroidTestCase{
+
+    public void testNotifyOnStatusChanged() {
+        ServerStatusChanger serverStatusChanger = new ServerStatusChanger(mContext);
+        ServerStatusListener statusListener = mock(ServerStatusListener.class);
+        serverStatusChanger.addListener(statusListener);
+
+        serverStatusChanger.fromResponseCode(HttpURLConnection.HTTP_OK);
+
+        verify(statusListener).onServerStatusChanged(ServerStatus.SERVER_STATUS_OK);
+    }
+
+    public void testStoreTheCorrectStatus() {
+        ServerStatusChanger serverStatusChanger = new ServerStatusChanger(mContext);
+
+        serverStatusChanger.fromResponseCode(HttpURLConnection.HTTP_OK);
+
+        assertEquals(ServerStatus.SERVER_STATUS_OK, serverStatusChanger.getServerStatus());
+    }
+
+    public void testOnResetShouldChangeTheStateToUnknown() {
+        ServerStatusChanger serverStatusChanger = new ServerStatusChanger(mContext);
+
+        serverStatusChanger.resetServerStatus();
+
+        assertEquals(ServerStatus.SERVER_STATUS_UNKNOWN, serverStatusChanger.getServerStatus());
+    }
+}

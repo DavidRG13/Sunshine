329592632a79e1b8c22b3d0387b06a6bca3b3edc
david <DAVIDrg?>
2015-07-20 10:05:46 +0100

    A little cleaning before starting "Ubiquitous Computing Course"

--- app/src/androidTest/java/com/android/sunshine/app/MainActivityTest.java
+++ app/src/androidTest/java/com/android/sunshine/app/MainActivityTest.java
@@ -25,7 +25,6 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItem;
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
